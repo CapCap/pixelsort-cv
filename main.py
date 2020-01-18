2b993877f36a1006e4c4b779b35cabbfdabb14e0
Max <maximilian.kaplan@gmail.com>
2020-01-17 23:25:00 -0800

    fix

--- main.py
+++ main.py
@@ -10,9 +10,25 @@ while True:
     # Capture frame-by-frame
     ret, frame = cap.read()
 
-    # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    # frame = imutils.auto_canny(gray, sigma=0.99)
-    frame = pixelsort(frame, interval_function="none")  # , interval_function="threshold", lower_threshold=0.5)
+    # print(frame.shape) # (720, 1280, 3)
+
+    # frame = pixelsort(frame, interval_function="none")  # , interval_function="threshold", lower_threshold=0.5)
+
+    window_size = 25
+    i = 0
+    for x in range(0, frame.shape[0], window_size):
+        #i += 1
+        for y in range(0, frame.shape[1], window_size):
+            frame[x:x + window_size, y:y + window_size].sort(i % 2 == 0)
+            i += 1
+
+    window_size *= 2
+    i = 0
+    for x in range(0, frame.shape[0], window_size):
+        i += 1
+        for y in range(0, frame.shape[1], window_size):
+            frame[x:x + window_size, y:y + window_size].sort(i % 2 == 0)
+            i += 1
 
     # Display the resulting frame
     cv2.imshow('frame', frame)
